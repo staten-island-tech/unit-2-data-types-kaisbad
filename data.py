fb2f606a401c6a9cbe911eb298a5bb1cb0f33417
kaisbad <kaiz8@nycstudents.net>
2024-02-05 12:28:59 -0500

    asd

---
+++ data.py
@@ -0,0 +1,49 @@
+#Data Types
+#Numbers 1,2,3
+""" def add(x,y):
+    print(x + y)
+add(1,2)
+#strings "a,b,c"
+name = "Mark"
+def greeting(person):
+    print(person)
+greeting(name) 
+#1 and "1" are not the same
+add("1","2")
+#undefined/null
+
+#booleans
+tenure = False
+def is_tenure(status):
+    if(status == True):
+        print("They have tenure")
+    else:
+        print("They are not tenured")
+    
+is_tenure(tenure)
+ """
+""" x = 3
+y = float(3)
+print(x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+
+print(values[0])
+print(values[6])
+ """
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z)
+ """
+day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect")
+
+
---
+++ lists.py
@@ -0,0 +1,19 @@
+animals = ["Zebra", "Camel", "Ape"]
+#start count at 0, reference each element with []
+# print (animals[0])
+# for animal in animals: 
+#     print(animal)
+
+#for animal in animals:
+#    if(animal =="Camel"):
+#        print("We're in the desert")
+
+#Strings operate like lists 
+x = "Hello Freshmen, you all smell"
+#print(x[0])
+#y = x.upper()
+#print(y)
+words_list = x.split( )
+print(len(words_list))
+
+
